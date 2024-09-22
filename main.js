f9864d563199eac17bde0e28ce586bc8682bad71
Yh010 <yash4success@gmail.com>
2024-09-22 09:35:03 +0530

    passage time counter

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
+const passage = `The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox nymphs grab quick-jived waltz. Brick quiz whangs jumpy veldt fox. Bright vixens jump; dozy fowl quack. Quick wafting zephyrs vex bold Jim. Quick zephyrs blow, vexing daft Jim. Sex-charged fop blew my junk TV quiz. How quickly daft jumping zebras vex. Two driven jocks help fax my big quiz. Quick, Baz, get my woven flax jodhpurs! "Now fax quiz Jack!" my brave ghost pled. Five quacking zephyrs jolt my wax bed. Flummoxed by job, kvetching W. zaps Iraq. Cozy sphinx waves quart jug of bad milk. The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox nymphs grab quick-jived waltz. Brick quiz whangs jumpy veldt fox. Bright vixens jump; dozy fowl quack. Quick wafting zephyrs vex bold Jim. Quick zephyrs blow, vexing daft Jim. Sex-charged fop blew my junk TV quiz. How quickly daft jumping zebras vex. Two driven jocks help fax my big quiz. Quick, Baz, get my woven flax jodhpurs! "Now fax quiz Jack!" my brave ghost pled. Five quacking zephyrs jolt my wax bed. Flummoxed by job, kvetching W. zaps Iraq. Cozy sphinx waves quart jug of bad milk.`
 
-let nums = ['2', '4', '25', '10', '3']; 
 
+function wordstime(para) {
+    para = para.split(" ").map(element => {
+        element = element.trim();
+    }).filter(element => element !== "");
 
-function convert(nums) {
-   return nums.map(num => {
-      return  +num < 10 ? `0${num}` : num;
-    })
+    let numWords = para.length;
+
+    let timeFactor = numWords / 200;
+
+    if (timeFactor < 1) {
+        return `${timeFactor*60} seconds`
+    } else {
+        return `${timeFactor} minutes`
+    } 
 }
 
-console.log(convert(nums))
+console.log(wordstime(passage))
