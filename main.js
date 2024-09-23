b75a8a438a963d6f0498e7b62ccdcbcab76e358d
Yh010 <yash4success@gmail.com>
2024-09-23 18:10:04 +0530

    rate limit

--- main.js
+++ main.js
@@ -4,45 +4,55 @@ const PORT = 3000;
 
 app.use(express.json());
 
-const products = [
-    { id: 1, name: 'Product 1', price: 10 },
-    { id: 2, name: 'Product 2', price: 20 },
-    { id: 3, name: 'Product 3', price: 30 },
-    { id: 4, name: 'Product 4', price: 40 },
-    { id: 5, name: 'Product 5', price: 50 },
-    { id: 6, name: 'Product 6', price: 60 },
-    { id: 7, name: 'Product 7', price: 70 },
-    { id: 8, name: 'Product 8', price: 80 },
-    { id: 9, name: 'Product 9', price: 90 },
-    { id: 10, name: 'Product 10', price: 100 },
-    { id: 11, name: 'Product 11', price: 110 },
-    { id: 12, name: 'Product 12', price: 120 },
-    // Add more products if needed...
-];
 
-app.get("/products", (req, res) => {
-  const page = parseInt(req.query.page) || 1;  // Default to page 1 if not provided
-  const limit = parseInt(req.query.limit) || 10;
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-  const totalPage = Math.ceil(products.length / limit);
+async function main() {
+  const start = new Date();
+  
+  await delay(10000);  // Wait for 10 seconds
+  
+  const end = new Date();
+  console.log((end - start)/1000);  // Prints the difference in milliseconds (should be around 10000)
+}
+
+let requestCount = 0;
+let firstTime;
 
 
-  const startIndex = (page - 1) * limit;
-  const endIndex = startIndex + limit;
+const timeoutLimit = 1000;
+
+const ratelimit = 5;
 
 
-  const display = products.slice(startIndex, endIndex);
 
-  const response = {
-    totalPage,
-    data: display,
-    page,
-    limit
+app.get("test", (req, res) => {
+  const { userid } = req.body;
+  const currentTme = new Date();
+
+
+  if (!firstTime) {
+    firstTime = currentTme;
   }
-  res.json(response)
+
+  if (currentTme - firstTime < timeoutLimit) {
+    requestCount++;
+
+   if (requestCount > ratelimit) {
+      // If the limit is exceeded, send a rate limit error
+      return res.status(429).json({ error: "Rate limit hit" });
+    }
+  } else {
+     firstTime = currentTme;
+    requestCount = 1; // Count this request
+  }
+  res.json({ message: "Request successful" });
 })
 
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
